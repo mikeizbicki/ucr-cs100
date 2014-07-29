ab83a14190740eb685894e735e6fac249603ac85
Mike Izbicki <mike@izbicki.me>
2014-07-29 15:47:58 -0700

    lecture 2 notes

---
+++ lecture/facebook/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    srand(0);
+
+    cout << "super simple" << endl;
+    cout << "random: " << rand()%100 << endl; 
+    return 0;
+}
