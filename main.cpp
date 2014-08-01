b464df45bc6fd178de36e6acd09f3d732f51ea6f
DanTheTingler <dting001@ucr.edu>
2014-07-31 20:09:59 -0700

    my first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string name;
+
+	cout << "What is your name" << endl;
+	cin >> name;
+
+	cout << "Hello " << name << "!" << endl;
+	
+	return 0;
+}
