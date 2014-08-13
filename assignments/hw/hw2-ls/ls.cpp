af69884d0c154f5a14c1472f4d9379b975345ad6
Mike Izbicki <mike@izbicki.me>
2014-08-13 10:26:11 -0700

    made error checking requirements more clear

--- assignments/hw/hw2-ls/ls.cpp
+++ assignments/hw/hw2-ls/ls.cpp
@@ -6,18 +6,19 @@
 
 using namespace std;
 
+/*
+ * This is a BARE BONES example of how to use opendir/readdir/closedir.  Notice
+ * that there is no error checking on these functions.  You MUST add error 
+ * checking yourself.
+ */
+
 int main()
 {
-   char *dirName = ".";
-   DIR *dirp;
-   if (!(dirp  = opendir(dirName)))
-    {
-       cerr << "Error(" << errno << ") opening " << dirName << endl;
-       return errno;
-   }
-   dirent *direntp;
-   while ((direntp = readdir(dirp)))
-       cout << direntp->d_name << endl;  // use stat here to find attributes of file
-   closedir(dirp);
+    char *dirName = ".";
+    DIR *dirp = opendir(dirName)
+    dirent *direntp;
+    while ((direntp = readdir(dirp)))
+        cout << direntp->d_name << endl;  // use stat here to find attributes of file
+    closedir(dirp);
 }
 
