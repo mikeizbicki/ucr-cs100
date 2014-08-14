36ebd6b9b5c1d1388688523510e712908169f116
Mike Izbicki <mike@izbicki.me>
2014-08-14 15:52:00 -0700

    lecture notes

---
+++ lecture/bashtest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#var =$(echo $(./a.out) )
+#echo "$var $var $var $var"
+#echo 1+2
+
+#if ./a.out; then
+#if [ -f test ]; then
+    #echo "hi"
+    #echo "test"
+#fi
+
+ls $1 $2  $1 $2 
+
+#for i in *; do
+    #echo $i
+#done
---
+++ lecture/return.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    cout << "hello!" << endl;
+    exit(0);
+    exit(1);
+    return 1;
+}
