79626c8ceffdec596d8dedd3b368540f73c99f26
vdoan002 <vdoan002@ucr.edu>
2014-10-20 00:15:49 -0700

    Command line works

--- assignments/hw/hw0-rshell/rshell.cpp
+++ assignments/hw/hw0-rshell/rshell.cpp
@@ -6,30 +6,17 @@
 #include <sys/types.h>
 #include <cstdlib>
 #include <stdio.h>
-#include <boost/tokenizer.hpp>
 
 using namespace std;
-//using namespace boost;
 
 int main()
 {
-	
 	cout << endl;
 	cout << "$";
 	string command;
 
 	getline(cin, command);
 
-	
-
-//	tokenizer<char_separator<char> > tok(command);
-//	char_separator<char> sep(" ", keep_empty_tokens);
-//	for(tokenizer<>::iterator
-//	 beg=tok.begin(); beg!=tok.end(); ++beg)
-//	{
-//		cout << *beg << endl;
-//	}
-		
 
 	int argvlength = 0;	
 	int i = 0;
@@ -53,13 +40,9 @@ int main()
 		argvlength++;
 		p = strtok(NULL," ");
 	}
-		argv[argvlength] = NULL;
-//	argv[0] = new char[3];
-	cout <<	argv[0] << endl;
-	cout <<	argv[1] << endl;
+	argv[argvlength] = NULL;
 
-	cout <<	argv[2] << endl;
-	cout << argv[3] << endl;
+
 	int r = execvp(argv[0], argv);
 
 	if(r == -1)
@@ -71,7 +54,5 @@ int main()
 	}
 
 	
-	
-
 	return 0;
 }
