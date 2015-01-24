f8006bff2f407b1d659ace6ca347ab40c9df7c33
Ace Rhino <a12n@mac.com>
2015-01-23 18:05:12 -0800

    Added tests and fixed bugs. Submission ready

--- assignments/hw/hw0-rshell/rshell.cpp
+++ assignments/hw/hw0-rshell/rshell.cpp
@@ -12,8 +12,50 @@
 #include <stdlib.h>
 #include <sstream>
 #include <vector>
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <locale>
 
 using namespace std;
+
+//the following are UBUNTU/LINUX ONLY terminal color codes.
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+//The following three functions are taken from the first answer on this question
+//http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// trim from start
+string &ltrim(string &s) {
+    s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+    return s;
+}
+
+// trim from end
+string &rtrim(string &s) {
+    s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
+    return s;
+}
+
+// trim from both ends
+string &trim(string &s) {
+    return ltrim(rtrim(s));
+}
 
 void quit(int exit_val) {
     cout << "\033[0m";
@@ -28,15 +70,20 @@ void my_handler(int s){
 //Run a command and do error checking
 //Return the return of the command (or 1 if it wasn't run)
 int run_cmd_(char *cmd, char **argv) {
-    if(strcmp(cmd, "exit") == 0) quit(0);
+    if(strcmp(cmd, "exit") == 0) {
+        cout << "\033[0m";
+        exit(0);
+    }
     int pid = fork();
     if(pid == -1) {
         perror("fork fail");
         return 1;
     } else if(pid == 0) {
         if(execvp(cmd,argv) != 0) {
-            cerr << "FAILED" << endl;
-            perror("error in execvp");
+            char err[7 + strlen(cmd) + 1];
+            strcpy(err, "-bqsh: ");
+            strcat(err, cmd);
+            perror(err);
         }
         return 1;
     } else {
@@ -64,11 +111,12 @@ int run_cmd(string line) {
     }
     argv[cmds_vec.size()] = NULL;
     
-    //for(int x=0; x<cmds_vec.size(); x++) {
-    //    cerr << argv[x] << endl;
-    //}
-    
-    return run_cmd_(argv[0], argv);
+    int result = run_cmd_(argv[0], argv);
+    for(int x=0; x<=cmds_vec.size(); x++) {
+        delete []argv[x];
+    }
+    delete []argv;
+    return result;
 }
 
 //Recursively parse and check the conditionals
@@ -94,23 +142,30 @@ void parse_conditional(char cmds[]) {
     vector<string> parsed;
     string s_cmds = cmds;
     
+    //Check if they lead with a conditional...
+    if(s_cmds.substr(0, 2) == "&&" || s_cmds.substr(0, 2) == "||") {
+        cerr << "Bad conditionals" << endl;
+        return;
+    }
+    
+    //Parse parse parse
     char prev = '\0';
     int last_found = 0;
     for(int i=0; i < s_cmds.size(); i++) {
         if(s_cmds.at(i) == prev && (prev == '&' || prev == '|')) {
             parsed.push_back(s_cmds.substr(last_found, i-last_found-1));
+            trim(parsed.at(parsed.size()-1));
+            if(parsed.at(parsed.size()-1) == "") parsed.pop_back();
             parsed.push_back(string(2, prev));
             last_found = i + 1;
         }
         prev = s_cmds.at(i);
     }
     parsed.push_back(s_cmds.substr(last_found, s_cmds.size() - last_found));
+    trim(parsed.at(parsed.size()-1));
+    if(parsed.at(parsed.size()-1) == "") parsed.pop_back();
     
-    //TODO:
-    //Fix case where all conditionals
-    //Fix case where ends in a conditional
-    //Fix case where starts in conditional
-    
+    //Output args for debugging
     //for(int x=0; x<parsed.size(); x++) {
     //    cout << parsed[x] << endl;
     //}
@@ -126,6 +181,9 @@ void parse_conditional(char cmds[]) {
 
 void parse_semi(char cmds[]) {
     if(cmds == NULL) return;
+    char space_test[strlen(cmds) + 1];
+    strcpy(space_test, cmds);
+    if(strtok(space_test, " \t\r\n") == NULL) return;
     
     char *parsed = strtok(cmds, ";");
     while(parsed != NULL) {
@@ -135,9 +193,7 @@ void parse_semi(char cmds[]) {
 }
 
 int main(int argc, char *argv[]) {
-    //Change terminal color for the rest of the program
-    cout << "\033[1m\033[35m";
-    
+
     //Setup handler to change color back if they do ctrl-c
     struct sigaction sigIntHandler;
     sigIntHandler.sa_handler = my_handler;
@@ -146,20 +202,35 @@ int main(int argc, char *argv[]) {
     sigaction(SIGINT, &sigIntHandler, NULL);
     
     
-	//while(true) {
-    
-    //Grab a line and convert it to a C-String
-    string cmds;
-    cout << "$ ";
-    getline(cin, cmds);
-    char cmd[cmds.size() + 1];
-    strcpy(cmd, cmds.c_str());
-    
-    //Cut out a comment if there is one and pass to the parsers
-    parse_semi(strtok(cmd, "#"));
-    
-    //run_cmd(cmd, argv);
-    
-    	//}*/
+	while(true) {
+        
+        //Grab Username
+        char uname[64] = "";
+        if(getlogin_r(uname, sizeof(uname)-1) != 0) {
+            perror("Error fetching Username");
+            strcpy(uname, "???");
+        }
+        
+        //Grab hostname
+        char hostname[64] = "";
+        if(gethostname(hostname, sizeof(hostname)-1) != 0) {
+            perror("Error fetching Hostname");
+            strcpy(hostname, "???");
+        }
+        
+        //Grab a line and convert it to a C-String
+        string cmds;
+        cout << GREEN << uname << "@" << YELLOW << hostname << RESET << "$ ";
+        getline(cin, cmds);
+        //cout << "\033[0m";
+        char cmd[cmds.size() + 1];
+        strcpy(cmd, cmds.c_str());
+        
+        if(cmds.at(0) == '#') continue;
+        
+        
+        //Cut out a comment if there is one and pass to the parsers
+        parse_semi(strtok(cmd, "#"));
+    }
     quit(0);
 }
