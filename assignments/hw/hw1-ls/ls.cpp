945880d8e4ea41580452b41d9cbcd62920ec8d50
scohe001 <scohe001@ucr.edu>
2015-01-28 22:46:49 -0800

    Fix warnings
    Declaring `dirName` as `char*` throws warnings that turn into errors when compiled with `-ansi -pedantic -Wall -Werror`

--- assignments/hw/hw1-ls/ls.cpp
+++ assignments/hw/hw1-ls/ls.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int main()
 {
-    char *dirName = ".";
+    char dirName[] = ".";
     DIR *dirp = opendir(dirName);
     dirent *direntp;
     while ((direntp = readdir(dirp)))
