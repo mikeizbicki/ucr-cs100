722a8daa752b1ca3ee9aedea29f9a4522f2d2db5
Mike Izbicki <mike@izbicki.me>
2015-04-22 12:02:48 -0700

    lecture notes

--- lecture-notes/exec.cpp
+++ lecture-notes/exec.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 using namespace std;
 
 int main(int argc, char **argv)
@@ -11,12 +14,15 @@ int main(int argc, char **argv)
 
     int i=fork();
     if (i==0) {
-        if (-1==execvp("asdasdasdadsls",argv)) {
+        if (-1==execvp("ls",argv)) {
            perror("execvp");
         }
     }
     else {
-        cout << "hello world!!!" << endl;
+        int status=0;
+        wait(&status);
+        cout << "status=" << status << endl;
+        while (1);
     }
 
     return 0;
