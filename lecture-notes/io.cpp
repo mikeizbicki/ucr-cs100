ab92744a053d87b81014732c5a838692855f00b5
Mike Izbicki <mike@izbicki.me>
2015-05-06 11:59:18 -0700

    lecture notes

--- lecture-notes/io.cpp
+++ lecture-notes/io.cpp
@@ -13,13 +13,24 @@ int main(int argc, char **argv)
     cerr << "fd[0] = " << fd[0] << endl;
     cerr << "fd[1] = " << fd[1] << endl;
 
-    if (-1==write(fd[1],"hello world", 11)) {
-        perror ("write");
+    int pid=fork();
+    if (pid==0) {
+        close(1);
+        dup(fd[1]);
+        if (-1==write(fd[1],"hello world", 11)) {
+            perror ("write");
+        }
+        cout << "goodbye world" << endl;
     }
-
-    char buf[BUFSIZ];
-    read(fd[0],buf,11);
-    cerr << "buf=" << buf << endl;
+    else if (pid>0) {
+        close(0);
+        dup(fd[0]);
+        string buf;
+        cin >> buf;
+        //char buf[BUFSIZ];
+        //read(fd[0],buf,BUFSIZ);
+        cerr << "buf=" << buf << endl;
+    }
 
     /*
     close(0);
